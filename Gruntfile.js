784258f2dc99036ddb3618ef14749442cc525a6b
Zachary Berardo <zachary@zachnthebox.com>
2017-10-19 08:54:12 -0400

    feat: add docker support for development (#13)
    * feat: add docker support for development
    * chore: add documentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,7 +55,7 @@ module.exports = function(grunt) {
         port: 9000,
         livereload: 35729,
         // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
+        hostname: process.env.HOSTNAME || 'localhost'
       },
       livereload: {
         options: {
